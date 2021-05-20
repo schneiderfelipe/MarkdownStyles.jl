b9612a4155571f976013761d6a461f559b41a13f
Documenter.jl <documenter@juliadocs.github.io>
2021-05-20 18:03:52 +0000

    build based on bc5683c

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MarkdownStyles","category":"page"},{"location":"#MarkdownStyles","page":"Home","title":"MarkdownStyles","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MarkdownStyles.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MarkdownStyles]","category":"page"}]
+}
